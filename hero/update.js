6130f55c598db30c1bf735c9f2509a2237a128c4
joseloaiza <joseloaiza815@gmail.com>
2022-08-14 13:07:03 -0400

    new serverless deploy update hero

---
+++ hero/update.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const { sendResponse } = require("../functions/index");
+const dynamoDb = require("../db");
+
+module.exports.updateHero = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
+
+    const { name, alias, specie, companyName, companyTeam, id } = body;
+    const params = {
+      TableName: process.env.DYNAMO_TABLE_NAME,
+      Key: {
+        id,
+      },
+      ExpressionAttributeValues: {
+        ":name": name,
+        ":alias": alias,
+        ":specie": specie,
+        ":companyName": companyName,
+        ":companyTeam": companyTeam,
+      },
+      UpdateExpression:
+        "SET name = :name, alias = :alias, specie = :specie, companyName = :companyName, companyTeam = :companyTeam",
+      ReturnValues: "ALL_NEW",
+    };
+
+    const data = await dynamoDb.update(params).promise();
+    if (data.Attributes) {
+      return sendResponse(200, data.Attributes);
+    } else {
+      return sendResponse(404, { message: "Updated hero data not found" });
+    }
+  } catch (e) {
+    return sendResponse(500, { message: "Could not update this hero" });
+  }
+};
