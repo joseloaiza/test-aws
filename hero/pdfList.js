b26562680566ebfde9be5d15566ed618fd7258e2
joseloaiza <joseloaiza815@gmail.com>
2022-08-15 14:35:09 -0400

    generate pdf

--- hero/pdfList.js
+++ hero/pdfList.js
@@ -5,31 +5,78 @@ const { sendResponse } = require("../functions/index");
 const dynamoDb = require("../db");
 const PDFKit = require("pdfkit");
 const AWS = require("aws-sdk");
+const SES = new AWS.SES({ region: process.env.REGION });
+const validator = require("email-validator");
+const nodemailer = require("nodemailer");
 //var S3 = new AWS.S3();
 module.exports.listHeroesPdf = async (event, context) => {
   try {
-    return new Promise((resolve) => {
-      const doc = new PDFKit();
-
-      doc.text("text");
-
-      const buffers = [];
-      doc.on("data", buffers.push.bind(buffers));
-      doc.on("end", () => {
-        const pdf = Buffer.concat(buffers);
-        const response = {
-          statusCode: 200,
-          headers: {
-            "Content-Type": "application/pdf",
-          },
-          body: pdf.toString("base64"),
-          isBase64Encoded: true,
-        };
-        resolve(response);
+    const doc = new PDFKit();
+    const mailTo = event.queryStringParameters.mail;
+    if (!validator.validate(mailTo)) {
+      return sendResponse(200, {
+        message: "Enter a valid email address",
       });
+    }
 
-      doc.end();
-    });
+    doc.text("text");
+    const buffers = [];
+    doc.on("data", buffers.push.bind(buffers));
+
+    const filename = `${process.env.PDF_FOLDER}/${date}.pdf`;
+    const params_s3 = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: filename,
+      Body: buffers,
+    };
+    //save the information in S3
+    await S3.upload(params_s3).promise();
+    const options = {
+      from: "joseloaiza815@gmail.com",
+      subject: "Hero Report",
+      to: mailTo,
+      text: "prueba reporte hero",
+      html: `<div>${"Report Hero"}</div>`,
+      attachments: [
+        {
+          filename: `${date}.pdf`,
+          content: buffers,
+        },
+      ],
+    };
+    const transporter = nodemailer.createTransport({ SES });
+    await transporter.sendMail(options);
+
+    // const promise = new Promise((resolve) => {
+    //   stream.on("finish", () => {
+    //     resolve();
+    //   })
+    // });
+
+    // return new Promise((resolve) => {
+    //   const doc = new PDFKit();
+
+    //   doc.text("text");
+
+    //   const buffers = [];
+    //   doc.on("data", buffers.push.bind(buffers));
+    //   doc.on("end", () => {
+    //     const pdf = Buffer.concat(buffers);
+    //     const response = {
+    //       statusCode: 200,
+    //       headers: {
+    //         "Content-Type": "application/pdf",
+    //       },
+    //       body: pdf.toString("base64"),
+    //       isBase64Encoded: true,
+    //     };
+    //     resolve(response);
+    //   });
+
+    //   doc.end();
+    //});
+
+    return sendResponse(200, { message: "OK" });
   } catch (e) {
     return sendResponse(500, {
       error: e.message,
